ee19434039063aa7d6bb29481577fbc946fdfa53
andrea <andreacduarteg@gmail.com>
2025-11-22 16:55:53 -0400

    Objetos JVS

---
+++ Documents/proyectouno/js/01Variables.js
@@ -0,0 +1,16 @@
+// Declaracion de Variables
+const nombre = "Andres"; // Declaracion de una constante que esperas que no cambie
+const lastName = "Garcia";
+let edad = 25; // Declaracion de una variable que puede cambiar
+var genero = "Masculino"; // YA NO SE USA VAR 
+let inStudent = true; // Declaracion de una variable booleana
+let precio = 19.99; // Declaracion de una variable numerica
+
+// Mostrar Variables
+
+alert(nombre); // es como el print
+alert(lastName);
+alert(edad);
+alert(genero);
+alert(inStudent);
+alert(precio);
